c657b99fae1d1f91d7749579e51b605b3bab95d4
Coop <coop.coding@gmail.com>
2018-09-29 18:40:37 +1000

    Fixed an issue with npm install with windows shell

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const path = require('path')
 const { spawn } = require('child_process')
+const os = require('os')
 
 const fs = require('fs-extra')
 const chalk = require('chalk')
@@ -9,6 +10,8 @@ const { getProgramArgs } = require('./argsParser.js')
 const { checkAppNameCanBeUsed } = require('./utils.js')
 const { createFiles } = require('./createFiles.js')
 const { generatePackagesListToInstall } = require('./generatePackagesListToInstall.js')
+
+const isWindows = os.platform() === 'win32'
 
 createApp(...getProgramArgs())
 
@@ -29,7 +32,19 @@ function createApp(projectName, programArgs) {
 
 function installNpmPackages(packagesToInstall){
   console.log('Installing packages.\n')
-  const child = spawn('npm', ['install', ...packagesToInstall ,'-D'], { stdio: 'inherit' });
+  const child = spawn(
+    'npm',
+    [
+      'install',
+      ...packagesToInstall ,
+      '-D'
+    ],
+    {
+      stdio: 'inherit',
+      // had some issues on windows, 'shell:true' seems to fix it
+      shell: isWindows ? true : false
+    }
+  )
   child.on('close', code => {
     if(code !== 0) {
       console.error('There was an error installing the packages.')
